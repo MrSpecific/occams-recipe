b7f3f3a0f221474815882bd1e3f84bae710c5f31
Will Christenson <will@wlcr.io>
2021-03-02 12:04:57 -0800

    Add colors and some styles

--- components/header.js
+++ components/header.js
@@ -1,11 +1,15 @@
 import styles from '../styles/Header.module.css'
 import siteInfo from '../lib/siteInfo';
 
+import Link from 'next/link';
+
 const Header = (props) => {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>
-        {props.title || siteInfo.title}
+        <Link href={'/'}>
+          <a>{props.title || siteInfo.title}</a>
+        </Link>
       </h1>
       {props.children}
     </header>
