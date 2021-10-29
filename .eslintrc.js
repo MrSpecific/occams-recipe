4e15ec82fcfa4dcfc0bf2e11aba49aa61b13b157
Will Christenson <will@wlcr.io>
2021-10-29 14:48:16 -0700

    Updated linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ['next', 'airbnb-base', 'plugin:react/recommended', 'prettier', 'prettier/prettier'],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    // NextJs specific fix: suppress errors for missing 'import React' in files for nextjs
+    'react/react-in-jsx-scope': 'off',
+    // NextJs specific fix: allow jsx syntax in js files
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-indent-props': 'off',
+    'react/jsx-uses-vars': 'error',
+    // 'react/prop-types': [2, { ignore: ['children'] }],
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'prettier/prettier': ['error'],
+  },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
         map: [
-          ["@components", "./components/"],
-          ["@data", "./data/"],
-          ["@pages", "./pages/"],
-          ["@public", "./public/"],
-          ["@styles", "./styles/"],
-          ["@utilities", "./utilities/"],
+          ['@components', './components/'],
+          ['@data', './data/'],
+          ['@pages', './pages/'],
+          ['@public', './public/'],
+          ['@styles', './styles/'],
+          ['@utilities', './utilities/'],
         ],
-        extensions: [".js", ".jsx"],
+        extensions: ['.js', '.jsx'],
       },
     },
   },
